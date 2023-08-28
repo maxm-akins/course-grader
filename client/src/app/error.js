e86c24286ae97a1de4d277927a7963bf8bc1a378
Maxm Akins <maxm.akins@gmail.com>
2023-08-28 12:33:42 -0400

    error page fixes

--- client/src/app/error.js
+++ client/src/app/error.js
@@ -10,7 +10,7 @@ export default function Error({ error, reset }) {
             <div className="text-center">
                 <p className="text-2xl font-semibold text-pink-400">Error</p>
                 <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Sorry, there was an unexpected error.</h1>
-                <p className="mt-6 text-base leading-7 text-gray-600">"{ error?.message }"</p>
+                {/* <p className="mt-6 text-base leading-7 text-gray-600">"{ error?.message }"</p> */ }
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <button onClick={ reset } className="text-sm font-semibold text-gray-900">
                         Retry
